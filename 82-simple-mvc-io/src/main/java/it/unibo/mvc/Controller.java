3ffb53f437b611f55643cb066bc04c3d7d28734e
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2022-11-15 11:23:38 +0100

    class Controller created

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,33 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private File currentFile = new File(System.getProperty("user.home") + File.separator + "output.txt");
+
+    public void setFile(final File file) {
+        this.currentFile = file;
+    }
+
+    public File getFile() {
+        return this.currentFile;
+    }
+
+    public String getPath() {
+        return this.currentFile.getPath();
+    }
+
+    public void printString(final String input) throws IOException {
+        try(final PrintStream ps = new PrintStream(this.currentFile.getPath(), StandardCharsets.UTF_8)) {
+            ps.println(input);
+        } 
+    }
 
 }
