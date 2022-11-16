9221c3a343d6bb643deebc867ff5e857e27f6193
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2022-11-16 14:48:24 +0100

    exercise 82 completed

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -12,20 +12,42 @@ public class Controller {
 
     private File currentFile = new File(System.getProperty("user.home") + File.separator + "output.txt");
 
+    /**
+     * Sets the input file as the current file.
+     * 
+     * @param file
+     */
     public void setFile(final File file) {
         this.currentFile = file;
     }
 
+    /**
+     * Returns the current file.
+     * 
+     * @return currentFile
+     */
     public File getFile() {
         return this.currentFile;
     }
 
+    /**
+     * Returns the path of the current file.
+     * 
+     * @return the path
+     */
     public String getPath() {
         return this.currentFile.getPath();
     }
 
+    /**
+     * Saves the string given as input on the current file.
+     * 
+     * @param input saved on the current file
+     * 
+     * @throws IOException
+     */
     public void printString(final String input) throws IOException {
-        try(final PrintStream ps = new PrintStream(this.currentFile.getPath(), StandardCharsets.UTF_8)) {
+        try (PrintStream ps = new PrintStream(this.currentFile.getPath(), StandardCharsets.UTF_8)) {
             ps.println(input);
         } 
     }
