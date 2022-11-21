aa22c242b3708c20ca91a336eb333c5e8fe7ddb1
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2022-11-21 17:19:38 +0100

    javadoc added

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,35 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * Application controller responsible of I/O access.
  */
 public interface Controller {
 
+    /**
+     * Sets the input string as the next string to print.
+     * 
+     * @param string
+     */
+    void setString(String string);
+
+    /**
+     * Returns the next string to print.
+     * 
+     * @return next string
+     */
+    String getString();
+
+    /**
+     * Returns the history of printed strings in for of a list.
+     * 
+     * @return history as list
+     */
+    List<String> getHistory();
+
+    /**
+     * Prints the current string.
+     */
+    void printString();
 }
