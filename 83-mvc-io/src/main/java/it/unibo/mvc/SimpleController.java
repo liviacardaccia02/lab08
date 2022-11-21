65d0158ca0145c71fe5095bd4bf95a7f7aaab183
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2022-11-21 17:28:30 +0100

    class SimpleController created

--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,36 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private final List<String> history = new ArrayList<>();
+    private String nextString;
+
+    @Override
+    public void setString(final String string) {
+        this.nextString = string;
+    }
+
+    @Override
+    public String getString() {
+        return this.nextString;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return this.history;
+    }
+
+    @Override
+    public void printString() throws IllegalStateException {
+        history.add(this.nextString);
+        System.out.println(this.nextString);
+    }
 
 }
